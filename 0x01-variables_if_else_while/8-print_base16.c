8123b1ee7d1d24519439bfd4b735c35a0a609583
Austin Obimbo <118368849+Obimbo07@users.noreply.github.com>
2023-01-02 02:31:57 +0300

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (success/correct)
+ */
+int main(void)
+{
+
+char base_16;
+
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
+}
+for (base_16 = 'a'; base_16 <= 'f'; base_16++)
+{
+putchar(base_16);
+}
+
+putchar('\n');
+
+return (0);
+
+}
